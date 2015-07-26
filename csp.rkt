e0dd150b23122e5e96ff61b58e436acfad2faade
César López-Natarén <cesarn@mindtouch.com>
2015-07-26 15:11:18 -0700

    Exercise the primitives

--- csp.rkt
+++ csp.rkt
@@ -5,6 +5,10 @@
 (define Toffee 'toffee)
 (define Choc 'chocolate)
 (define Bleep 'bleep)
+(define Around 'around)
+(define Up 'up)
+(define Down 'down)
+
 (define (stop arg) Bleep)
 
 (define (prefix event process)
@@ -35,3 +39,17 @@
 
 ; Instance of a recursively defined process
 (define coin-then-choc (((VM) Coin) Choc))
+
+
+; Example of mutually recursively defined process
+(define CT
+    (lambda (n)
+        (if (= n 0)
+            (choice2 Around (lambda () (CT 0))
+                     Up (lambda () (CT 1)))
+            (choice2 Up (lambda () (CT (+ n 1)))
+                     Down (lambda () (CT (- n 1)))))))
+
+; Instance of a mutually recursively defined process
+(define around-upup-downdown-down ((((((((((((CT 0) Around)) Up)) Up)) Down)) Down)) Down))
+
